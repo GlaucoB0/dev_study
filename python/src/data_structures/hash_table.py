cc074c594ad90dca6f3bf2631e778729d9d3baec
Glauco Soares <oniiichaan61@gmail.com>
2025-04-28 13:20:41 -0300

    hash explanation

---
+++ python/src/data_structures/hash_table.py
@@ -0,0 +1,5 @@
+#
+# É atribuição de chave e valor, é O(1) na chamada de um valor, na remoção e na adição
+# a chave leva diretamente para a locação da memoria onde está o valor por meio de uma
+# função hash
+#
